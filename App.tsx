4384d604bdce13395ad7c4571ea6a54fc4e4eedc
Razin Idzuddin <razin99@gmail.com>
2021-09-14 18:16:46 +1000

    Add apollo provider to provide hooks to client in other children components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import React from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,18 +8,18 @@ import Login from "./pages/Login";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "http://7482-115-131-129-206.ngrok.io/graphql",
+  });
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
+  );
 }
