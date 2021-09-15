cd677e2425e99c83efd39f7d933e6a1f7b3d5bc1
Razin Idzuddin <razin99@gmail.com>
2021-09-15 17:05:52 +1000

    Added case when there are no events

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,15 +1,18 @@
 import { useQuery } from "@apollo/client";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { NativeStackNavigationProp, NativeStackScreenProps } from "@react-navigation/native-stack";
 import gql from "graphql-tag";
 import React from "react";
 import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { RootStackParams } from "../App";
+import { CenterText } from "../components/CenterText";
 
 interface Event {
   id: string;
   title: string;
-  date: Date;
+  date: number;
   fee: number;
   location: string;
+  organizer: { username: string };
 }
 interface Events {
   events: Event[];
@@ -29,50 +32,48 @@ const ALL_EVENTS = gql`
   }
 `;
 
-interface HomeProp {
-  navigation: NativeStackScreenProps<any,any>;
-}
-export default function Home({ navigation }:HomeProp) {
+type HomeProp = NativeStackScreenProps<RootStackParams, "Home">;
+export default function Home({ navigation }: HomeProp): JSX.Element {
   const { loading, error, data } = useQuery<Events>(ALL_EVENTS, { pollInterval: 1000 });
   if (loading) return <CenterText>Loading...</CenterText>;
   if (error) return <CenterText>ERR!:: {error.message}</CenterText>;
   // TODO paginate instead of normal scroll
+  if (data && data.events.length === 0) return <CenterText>No events yet</CenterText>;
   return (
     <ScrollView>
-      {data && data.events.map((event: any) => {
-        return (
-          <Card key={event.id} navigation={navigation} eventId={event.id}>
-            <View style={styles.row}>
-              <Text style={styles.title} numberOfLines={1}>{event.title}</Text>
-              <Text style={styles.price} numberOfLines={1}>${event.fee}</Text>
-            </View>
-            <View style={{ ...styles.row, marginTop: 16 }}>
-              <Text numberOfLines={1}>Organized by: {event.organizer.username}</Text>
-              <Text numberOfLines={1}>{new Date(event.date * 1000).toDateString()}</Text>
-            </View>
-            <Text numberOfLines={1}>Location: {event.location}</Text>
-          </Card>
-        );
-      })}
+      {data &&
+        data.events.map((event: Event) => {
+          return (
+            <Card key={event.id} navigation={navigation} eventId={event.id}>
+              <View style={styles.row}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {event.title}
+                </Text>
+                <Text style={styles.price} numberOfLines={1}>
+                  ${event.fee}
+                </Text>
+              </View>
+              <View style={{ ...styles.row, marginTop: 16 }}>
+                <Text numberOfLines={1}>Organized by: {event.organizer.username}</Text>
+                <Text numberOfLines={1}>{new Date(event.date * 1000).toDateString()}</Text>
+              </View>
+              <Text numberOfLines={1}>Location: {event.location}</Text>
+            </Card>
+          );
+        })}
     </ScrollView>
   );
 }
 
-const CenterText = ({ children }: { children: React.ReactNode }) => (
-  <View style={styles.container}>
-    <Text>{children}</Text>
-  </View>
-);
-
 interface CardProp {
   children: React.ReactNode;
-  navigation?: NativeStackScreenProps<any,any>;
-  eventId?: string;
+  eventId: string;
+  navigation: NativeStackNavigationProp<RootStackParams, "Home">;
 }
 const Card = ({ children, navigation, eventId }: CardProp) => {
   const handleCardTap = () => {
-    if (!eventId || !navigation) return;
-  }
+    navigation.navigate("Event", { eventId });
+  };
   return (
     <TouchableOpacity style={styles.card} onPress={handleCardTap}>
       {children}
@@ -81,14 +82,6 @@ const Card = ({ children, navigation, eventId }: CardProp) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 16,
-    borderRadius: 10,
-  },
   card: {
     padding: 16,
     marginTop: 16,
@@ -107,6 +100,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   price: {
-    fontSize: 32
-  }
+    fontSize: 32,
+  },
 });
