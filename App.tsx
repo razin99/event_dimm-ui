85f0395ee95cfa61cb059c3bf3d337b0db58ec3d
Razin Idzuddin <razin99@gmail.com>
2021-09-15 17:04:25 +1000

    Hardcoded userId 'token' and use wrap screens with AuthContext

--- App.tsx
+++ App.tsx
@@ -4,22 +4,35 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import React from "react";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Event from "./pages/Event";
+import { AuthContext } from "./AuthContext";
 
-const Stack = createNativeStackNavigator();
+export type RootStackParams = {
+  Home: undefined;
+  Login: undefined;
+  Event: { eventId: string };
+};
+const Stack = createNativeStackNavigator<RootStackParams>();
 
-export default function App() {
+export default function App(): JSX.Element {
+  const url = "http://1478-115-131-129-206.ngrok.io";
+  const userId = "a11c467e-b9f7-4267-85d7-d72dbc2d3ee0";
+
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: "http://7482-115-131-129-206.ngrok.io/graphql",
+    uri: `${url}/graphql`,
   });
   return (
     <ApolloProvider client={client}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AuthContext.Provider value={userId}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Event" component={Event} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AuthContext.Provider>
     </ApolloProvider>
   );
 }
